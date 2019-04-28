3826efaafe2956f82b71cb00b8034b6978e28d96
Abdul <awsayed@alaska.edu>
2019-04-28 08:01:46 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -6,3 +6,11 @@ def countdown(number)
   end
   return "HAPPY NEW YEAR!"
 end
+
+
+def countdown_with_sleep(number)
+  while number >= 10 do
+  "#{number} SECOND(S)!"
+  end
+  return "HAPPY NEW YEAR!"
+end
